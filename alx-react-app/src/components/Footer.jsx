9703fb3b61d0e18ad9a5532576dea0d91b000f89
Yakubu Tasiru <yakmtasir@gmail.com>
2025-11-14 21:21:22 +0100

    Added a inline css

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -1,8 +1,9 @@
 function Footer() {
-    return (
-        <footer>
-            <p>© 2023 City Lovers</p>
-        </footer>
-    );
+  return (
+    <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '10px' }}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
 }
+
 export default Footer;
--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,8 +1,9 @@
 function MainContent() {
-    return (
-        <main>
-            <p>I love to visit New York, Paris, and Tokyo.</p>
-        </main>
-    );
+  return (
+    <main style={{ backgroundColor: '#f0f0f0', color: '#333', padding: '20px', textAlign: 'center' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
 }
+
 export default MainContent;
