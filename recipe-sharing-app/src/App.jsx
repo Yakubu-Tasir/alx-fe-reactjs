f6916ee8b5ad4ae2164c37f1c962526abe428036
Yakubu Tasir <yakmtasir@gmail.com>
2025-11-23 17:12:52 -0800

    modifed

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,43 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 import EditRecipeForm from './components/EditRecipeForm'
-import DeleteRecipeButton from './components/DeleteRecipeButton';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Router>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src="./assets/react.svg" className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1> Recipe Sharing App </h1>
 
-      <AddRecipeForm />
-      <RecipeList />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <h1>Recipe Sharing App</h1>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <AddRecipeForm />
+              <RecipeList />
+            </>
+          }
+        />
+
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
+        <Route path="/recipes/:id/edit" element={<EditRecipeForm />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,14 +1,11 @@
 import useRecipeStore from "./recipeStore";
-import { useNavigate } from "react-router-dom";
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
-  const navigate = useNavigate();
 
   const handleDelete = () => {
     deleteRecipe(recipeId);
-    alert("Recipe deleted!");
-    navigate("/"); // Go back to home
+    alert("Recipe deleted");
   };
 
   return <button onClick={handleDelete}>Delete Recipe</button>;
