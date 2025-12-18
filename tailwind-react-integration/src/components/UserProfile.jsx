174f68ecbc4f8b72099566e86d8ab01ea21b3f27
Yakubu Tasir <yakmtasir@gmail.com>
2025-12-18 04:17:16 +0100

    function UserProfile() {   return (     <div       className=

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,11 @@ function UserProfile() {
         sm:p-4 md:p-8
         sm:max-w-xs md:max-w-sm
         mx-auto my-20
-        rounded-lg shadow-lg
+        rounded-lg
+        shadow-lg
+        hover:shadow-xl
+        transition-shadow
+        duration-300
       "
     >
       <img
@@ -17,6 +21,10 @@ function UserProfile() {
           sm:w-24 sm:h-24
           md:w-36 md:h-36
           mx-auto
+          transition-transform
+          duration-300
+          ease-in-out
+          hover:scale-110
         "
       />
 
@@ -26,6 +34,9 @@ function UserProfile() {
           text-blue-800
           my-4
           text-center
+          transition-colors
+          duration-300
+          hover:text-blue-500
         "
       >
         John Doe
